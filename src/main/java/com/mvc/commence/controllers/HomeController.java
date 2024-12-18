ec9ce661f4854e04fb8e2d5d9790160d336457d0
Pavlo Poliak <pavlo_poliak@epam.com>
2024-12-18 20:12:14 +0200

    update to spring boot

--- src/main/java/com/mvc/commence/controllers/HomeController.java
+++ src/main/java/com/mvc/commence/controllers/HomeController.java
@@ -1,15 +1,13 @@
-package com.mvc.web;
+package com.mvc.commence.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String goHome() {
+    public String goHome(){
         return "index";
     }
 }
